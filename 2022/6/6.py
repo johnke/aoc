9f905df7774c015de5f3910c2855d3f09a112d50
John Kelly <johnke@gmail.com>
2022-12-06 10:27:49 +0000

    Easy refactor

--- 2022/6/6.py
+++ 2022/6/6.py
@@ -4,24 +4,15 @@ test_input = open("sample.txt").read()
 input = open("input.txt").read()
 
 
-def partA(input):
+def partA(input, size=4):
   line = input.rstrip().rstrip()
   for i in range(0,len(line)):
-    sl = list(line[i:i+4])
+    sl = list(line[i:i+size])
     if len(list(set(sl))) == len(sl):
-      return i+4
+      return i+size
 
 print("test partA:", partA(test_input))
 print("partA:", partA(input))
 
-
-def partB(input):
-  line = input.rstrip().rstrip()
-  for i in range(0,len(line)):
-    sl = list(line[i:i+14])
-    if len(list(set(sl))) == len(sl):
-      return i+14
-
-
-print("test partB:", partB(test_input))
-print("partB:", partB(input))
+print("test partB:", partA(test_input, 14))
+print("partB:", partA(input, 14))
